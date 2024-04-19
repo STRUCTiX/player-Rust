a2da370c53458292a71238fc70c16a147098f01f
Timo Schneider <timoschneider04@gmail.com>
2024-04-19 19:50:50 +0200

    corrected units_until_upgrade type

--- src/models/base.rs
+++ src/models/base.rs
@@ -1,14 +1,13 @@
 use serde::Deserialize;
 use crate::models::position::Position;
-use crate::models::progress::Progress;
 
 #[derive(Deserialize)]
 pub struct Base {
-    pub position: Position,
-    pub uid: u32,
-    pub player: u32,
-    pub population: u32,
-    pub level: u32,
+    pub position: Position,          // position of the base
+    pub uid: u32,                    // uid of the base
+    pub player: u32,                 // owner of the base
+    pub population: u32,             // current population of the base
+    pub level: u32,                  // level of the base
     #[serde(rename="unitsUntilUpgrade")]
-    pub units_until_upgrade: Progress,
+    pub units_until_upgrade: u32,    // number of units required to upgrade
 }
