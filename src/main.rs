b6ef04e5967bcdb69febe0cb4ede2371d4a1e8ae
structix <janek@schoff.it>
2024-05-08 21:57:58 +0200

    Add axum dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(non_snake_case)]
+use axum::{routing::post, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", post(index));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
+
+async fn index() {}
