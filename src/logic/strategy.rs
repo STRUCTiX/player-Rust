d2549a0767c9c2d75fb4e9251d96cd84bb7c6fa8
structix <janek@schoff.it>
2024-05-17 20:20:04 +0200

    Add simple strategy

--- src/logic/strategy.rs
+++ src/logic/strategy.rs
@@ -1,7 +1,28 @@
 use crate::models::{game_state::GameState, player_action::PlayerAction};
 
 pub fn decide(game_state: GameState) -> Vec<PlayerAction> {
-    // TODO: place your player logic here.
+    let mut ogermeister: Vec<PlayerAction> = vec![];
+
+    for oger in &game_state.bases {
+        if oger.player != game_state.game.player {
+            continue;
+        }
+
+        for target in &game_state.bases {
+            if target.player == game_state.game.player {
+                continue;
+            }
+
+            let sub_oger = PlayerAction {
+                src: oger.uid,
+                dest: target.uid,
+                amount: target.population * 2 + 4,
+            };
+
+            ogermeister.push(sub_oger);
+        }
+    }
+
     vec![PlayerAction {
         src: 0,
         dest: 0,
